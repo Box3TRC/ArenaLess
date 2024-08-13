e44c8ff0a66b6503013ba3478cc3cbe614a64d01
tobylai-toby <toby_lai@126.com>
2024-08-13 19:22:44 +0800

    initial fixes

--- src/web/account.ts
+++ src/web/account.ts
@@ -95,13 +95,3 @@ export class Dao3Account {
     return data.code==200;
   }
 }
-// let account = new Dao3Account(
-//   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0eXAiOiJBQ0NFU1NfVE9LRU4iLCJpc3MiOiJjb2RlX2F1dGhfc2VydmljZSIsInN1YiI6Ijc0IiwianRpIjoiNTdjMDcyNWYtYzA3NS00NWFlLWE5ZmItM2M1M2MxZWMwN2RkIiwiaWF0IjoxNzIxOTc1MDM1LCJjaWQiOjEsImFodCI6NTEyLCJ1c2giOiIzNjJkN2ZlM2Q4YjI1ODFiZmZhMzU5ZjBlZWRhNzEwNiJ9.qEj5oGHCm2Rbqcwf81jFCH10t-osjN7Ojsdjj821tyA",
-//   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
-// );
-// account.getUserData().then((data) => {
-//   console.log(data);
-//   account.getExtMaps().then((data) => {
-//     console.log(data);
-//   });
-// });
